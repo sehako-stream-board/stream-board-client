cec25a97768b029d12e7a1c6ba8e764d20ee71c5
sehako <dhtpgkr1999@gmail.com>
2025-11-21 23:35:48 +0900

    refactor(board): 게시글 추가 버튼 내부 텍스트 정렬 및 색상 조정

--- src/static/AddPageButton.jsx
+++ src/static/AddPageButton.jsx
@@ -3,9 +3,12 @@ import { Button, Col, Row } from 'react-bootstrap';
 function AddPageButton() {
   return (
     <Row>
-      <Col className="position-absolute bottom-0 end-0 ms-3 mt-3 mb-3 me-1 text-end align-items-center">
-        <Button variant="dark" className="rounded-4 text-center ">
-          <span className="fs-3 fw-bold">+</span>
+      <Col className="position-absolute bottom-0 end-0 ms-3 mt-3 mb-3 me-1 text-end">
+        <Button
+          variant="dark"
+          className="align-items-center justify-content-center rounded-4 text-center bg-dark"
+        >
+          <span className="fs-1 fw-bold lh-1">+</span>
         </Button>
       </Col>
     </Row>
